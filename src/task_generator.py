f6056bc224aed818e27722370b0aeb8f9402bc26
Aleksandr Kochurov <laborkochurov@gmail.com>
2025-03-24 21:10:39 +0300

    [Issue #13] Implemented a method for generating a task condition

---
+++ src/task_generator.py
@@ -0,0 +1,35 @@
+import os
+
+
+def get_task_description(task_name, seed=None):
+    """
+    Возвращает текст условия задания для указанного названия задания и seed.
+
+    Args:
+        task_name (str): Название задания.
+        seed (int): Seed для выбора варианта задания. Если не указан, используется значение по умолчанию (None).
+
+    Returns:
+        str: Текст условия задания.
+
+    Raises:
+        FileNotFoundError: Если файл с условием задания не найден.
+    """
+    base_path = os.path.dirname(__file__)
+    task_path = os.path.join(base_path, 'tasks', task_name + '.txt')
+
+    if not os.path.exists(task_path):
+        raise FileNotFoundError(f"Описание задания для '{task_name}' не найдено.")
+
+    with open(task_path, 'r', encoding='utf-8') as file:
+        description = file.read()
+
+    return description
+
+
+def main():
+    print(get_task_description('basics'))
+
+
+if __name__ == '__main__':
+    main()
