51b6adab233a421879308b4c5e0e9dd59233df08
mishIgr <fmikhailv@gmail.com>
2025-04-10 23:36:04 +0300

    [Issue #35] add route dir

--- src/tasks/__init__.py
+++ src/tasks/__init__.py
@@ -1,4 +1,10 @@
+from pathlib import Path
 from .store_task import StoreTask
+
+ROOT_DIR = Path(__file__).parent.parent.parent.resolve()
+TEMPLATES_DIR = ROOT_DIR / 'templates'
+SETTINGS_DIR = ROOT_DIR / 'settings'
+
 from .implemented_tasks import *
 
 __all__ = [
