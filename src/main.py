8461bdfadcf381dac29a6f93457c41289cc3b040
mishIgr <fmikhailv@gmail.com>
2025-05-11 16:54:41 +0300

    [Issue #44] fix bag seed

--- src/main.py
+++ src/main.py
@@ -30,31 +30,30 @@ class App:
             "--seed", help="seed generation number", type=int
         )
 
-    def __print_generate_data(self, name, method):
+    def __print_generate_data(self, name, methods):
         task = self.store_task.get_task(name, self.args.seed)
-        match method:
-            case "code_tmp":
+        for method in methods:
+            if method == "code_tmp":
                 print((
                     f"Coderunner, name: {name}:\n"
                     f"{task.get_code_template()}"
                 ))
-            case "cond_task":
+            elif method == "cond_task":
                 print((
                     f"Condition, name: {name}:\n"
                     f"{task.get_condition_task()}"
                 ))
-            case "tmp_coderunner":
+            elif method == "tmp_coderunner":
                 print((
                     f"Code, name: {name}:\n"
                     f"{task.get_template_coderunner()}"
                 ))
-            case _:
+            else:
                 print("Такого метода не существует!")
 
     def run(self):
-        for method in self.args.method:
-            for name in self.args.name:
-                self.__print_generate_data(name, method)      
+        for name in self.args.name:
+            self.__print_generate_data(name, self.args.method)
 
 
 if __name__ == "__main__":
