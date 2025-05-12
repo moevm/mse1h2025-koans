8d813fc4063dde9716fb464e32d44e0acbbb1af9
Aleksandr Kochurov <laborkochurov@gmail.com>
2025-05-12 03:26:54 +0300

    [Issue #28] added changes to tasks

--- src/tasks/implemented_tasks/__init__.py
+++ src/tasks/implemented_tasks/__init__.py
@@ -1,5 +1,5 @@
 from .basic_task import BasicTask
-from src.tasks.implemented_tasks.pointers_tasks.function_pointers_task import FunctionPointersTask
+from .function_pointers_task import FunctionPointersTask
 from .pointers_and_addresses_task import PointersAndAddressesTask
 from .pointers_as_function_arguments_task import PointersAsFunctionArgumentsTask
 from .pointers_arrays_and_arithmetic_task import PointersArraysAndArithmeticTask
